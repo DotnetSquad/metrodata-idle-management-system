eaa272aeec220279c34c9d444127d36ae45d9bf6
putuagra <iputuagra@gmail.com>
2023-07-20 21:44:39 +0700

    feat: create placement model

---
+++ API/Models/Placement.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+[Table("tb_m_placements")]
+public class Placement : BaseEntity
+{
+    [Column("title", TypeName = "nvarchar(255)")]
+    public string Title { get; set; }
+
+    [Column("description", TypeName = "nvarchar(255)")]
+    public string Description { get; set; }
+
+    [Column("employee_guid")]
+    public Guid EmployeeGuid { get; set; }
+
+    [Column("profile_guid")]
+    public Guid ProfileGuid { get; set; }
+
+    // Cardinality
+    /*public Employee? Employee { get; set; }
+    public ICollection<Company>? Company { get; set; }*/
+}
