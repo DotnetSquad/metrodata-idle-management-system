0d517a43583e628300f41b25b4f41cb842d47978
rifkymaulana <rifkymaulanaindonesia@gmail.com>
2023-07-20 21:25:56 +0700

    feat: create employee model

---
+++ API/Models/Employee.cs
@@ -0,0 +1,47 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using API.Utilities.Enums;
+
+namespace API.Models;
+
+[Table("tb_m_employees")]
+public class Employee
+{
+    [Column("nik", TypeName = "nvarchar(6)")]
+    public string Nik { get; set; }
+
+    [Column("first_name", TypeName = "nvarchar(100)")]
+    public string FirstName { get; set; }
+
+    [Column("last_name", TypeName = "nvarchar(100)")]
+    public string? LastName { get; set; }
+    
+    [Column("birth_date")]
+    public DateTime BirthDate { get; set; }
+    
+    [Column("gender")]
+    public GenderEnum Gender { get; set; }
+    
+    [Column("hiring_date")]
+    public DateTime HiringDate { get; set; }
+    
+    [Column("status")]
+    public StatusEnum StatusEnum { get; set; }
+
+    [Column("email", TypeName = "nvarchar(50)")]
+    public string Email { get; set; }
+    
+    [Column("phone_number", TypeName = "nvarchar(20)")]
+    public string PhoneNumber { get; set; }
+    
+    [Column("grade_guid")]
+    public Guid GradeGuid { get; set; }
+
+    [Column("profile_guid")]
+    public Guid ProfileGuid { get; set; }
+    
+    public Account? Account { get; set; }
+    
+    // public Grade? Grade { get; set; }
+    
+    // public Profile? Profile { get; set; }
+}
