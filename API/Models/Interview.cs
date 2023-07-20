8b7e9653010c5e3fe02f3b81bc07987fea9e0af9
putuagra <iputuagra@gmail.com>
2023-07-20 22:26:36 +0700

    feat: create interview model

---
+++ API/Models/Interview.cs
@@ -0,0 +1,30 @@
+﻿using API.Utilities.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+[Table("tb_m_interviews")]
+public class Interview : BaseEntity
+{
+    [Column("job_name", TypeName = "nvarchar(255)")]
+    public string JobName { get; set; }
+
+    [Column("link", TypeName = "nvarchar(255)")]
+    public string? Link { get; set; }
+
+    [Column("interview_date")]
+    public DateTime InterviewDate { get; set; }
+
+    [Column("description", TypeName = "nvarchar(255)")]
+    public string Description { get; set; }
+
+    [Column("status")]
+    public StatusInterviewEnum Status { get; set; }
+
+    [Column("job_guid")]
+    public Guid JobGuid { get; set; }
+
+    // Cardinality
+    /*public Job? Job { get; set; }
+    public ICollection<EmployeeInterview>? EmployeeInterviews { get; set; }*/
+}
