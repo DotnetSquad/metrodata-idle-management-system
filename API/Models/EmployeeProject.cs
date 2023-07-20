9c975b3d01f9d89a51a008cbfbbea010806b194c
aefshaeful <aefsaefulrohma042@gmail.com>
2023-07-20 22:41:25 +0700

    feat: create employee project model API

---
+++ API/Models/EmployeeProject.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+[Table("tb_m_employee_project")]
+public class EmployeeProject : BaseEntity
+{
+    [Column("employee_guid")]
+    public Guid EmployeeGuid { get; set; }
+
+    [Column("project_guid")]
+    public Guid ProjectGuid { get; set; }
+}
