bd354b676a4f5621a0eefd9055317295ebcbbeda
rifkymaulana <rifkymaulanaindonesia@gmail.com>
2023-07-20 16:45:55 +0700

    feat: create role page

---
+++ Client/Controllers/RoleController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers;
+
+public class RoleController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
