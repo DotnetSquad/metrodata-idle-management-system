e6b0eaf0c0e3eeb4083e08d445963df20292b5f9
aefshaeful <aefsaefulrohma042@gmail.com>
2023-07-20 21:50:11 +0700

    feat: create model profile API

---
+++ API/Models/Profile.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+[Table("tb_m_profiles")]
+public class Profile : BaseEntity
+{
+    [Column("skills")]
+    public string skills { get; set; }
+
+    [Column("linkedin")]
+    public string linkedin { get; set; }
+
+    [Column("resume")]
+    public string resume { get; set; }
+}
