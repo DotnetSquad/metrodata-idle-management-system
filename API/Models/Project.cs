d891a3df5e81ff01ba8fc259b3c75ce84ee7c11d
aefshaeful <105156900+aefshaeful@users.noreply.github.com>
2023-07-20 22:53:44 +0700

    feat: create project model API (#64)

---
+++ API/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+[Table("tb_m_projects")]
+public class Project : BaseEntity
+{
+    [Column("name_project")]
+    public string NameProject { get; set; }
+
+    [Column("project_lead")]
+    public string ProjectLead { get; set; }
+
+    [Column("description")]
+    public string Description { get; set; }
+}
