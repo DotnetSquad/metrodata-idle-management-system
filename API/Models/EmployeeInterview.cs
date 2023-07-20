a11023d43ed09a89234cdea8083e290d4ef33530
rifkymaulana <rifkymaulanaindonesia@gmail.com>
2023-07-20 22:32:04 +0700

    chore: add base entity

--- API/Models/EmployeeInterview.cs
+++ API/Models/EmployeeInterview.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace API.Models;
 
 [Table("tb_tr_employee_interview")]
-public class EmployeeInterview
+public class EmployeeInterview : BaseEntity
 {
     [Column("employee_guid")]
     public Guid EmployeeGuid { get; set; }
