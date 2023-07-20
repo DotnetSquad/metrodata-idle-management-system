903c3999eca43ef3bb6802056f4c69930c132903
putuagra <iputuagra@gmail.com>
2023-07-20 22:13:41 +0700

    feat: create job model

---
+++ API/Models/Job.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+[Table("tb_m_jobs")]
+public class Job : BaseEntity
+{
+    [Column("job_name", TypeName = "nvarchar(255)")]
+    public string JobName { get; set; }
+
+    [Column("description", TypeName = "nvarchar(255)")]
+    public string Description { get; set; }
+
+    [Column("company_guid")]
+    public Guid CompanyGuid { get; set; }
+
+    // Cardinality
+    /*public Company? Company { get; set; }
+    public ICollection<Interview>? Interviews { get; set; }*/
+}
