cd9d2c64d6794effafd5222fa5e4c64ebcf637ae
putuagra <iputuagra@gmail.com>
2023-07-21 16:20:25 +0700

    feat: create placements dto

---
+++ API/DataTransferObjects/Placements/PlacementDtoGet.cs
@@ -0,0 +1,10 @@
+﻿namespace API.DataTransferObjects.Placements;
+
+public class PlacementDtoGet
+{
+    public Guid Guid { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Guid EmployeeGuid { get; set; }
+    public Guid CompanyGuid { get; set; }
+}
