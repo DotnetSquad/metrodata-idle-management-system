c6d9906b3524776f04d6451a1f713f9f449a5576
aefshaeful <105156900+aefshaeful@users.noreply.github.com>
2023-07-21 17:02:28 +0700

    feat: create company data transfer object (#121)

---
+++ API/DataTransferObjects/Companies/CompanyDtoCreate.cs
@@ -0,0 +1,8 @@
+﻿namespace API.DataTransferObjects.Companies;
+
+public class CompanyDtoCreate
+{
+    public string CompanyName { get; set; }
+    public string Description { get; set; }
+    public string Address { get; set; }
+}
