1063d9398f2c1f13a6b10cc33e14551cca8d49dc
putuagra <iputuagra@gmail.com>
2023-07-21 22:18:32 +0700

    feat: create implicit explicit company dtos

--- API/DataTransferObjects/Companies/CompanyDtoCreate.cs
+++ API/DataTransferObjects/Companies/CompanyDtoCreate.cs
@@ -1,8 +1,32 @@
-﻿namespace API.DataTransferObjects.Companies;
+﻿using API.Models;
+
+namespace API.DataTransferObjects.Companies;
 
 public class CompanyDtoCreate
 {
     public string CompanyName { get; set; }
     public string Description { get; set; }
     public string Address { get; set; }
+
+    // implicit operator
+    public static implicit operator Company(CompanyDtoCreate companyDtoCreate)
+    {
+        return new Company
+        {
+            CompanyName = companyDtoCreate.CompanyName,
+            Description = companyDtoCreate.Description,
+            Address = companyDtoCreate.Address
+        };
+    }
+
+    // explicit operator
+    public static explicit operator CompanyDtoCreate(Company company)
+    {
+        return new CompanyDtoCreate
+        {
+            CompanyName = company.CompanyName,
+            Description = company.Description,
+            Address = company.Address
+        };
+    }
 }
--- API/DataTransferObjects/Companies/CompanyDtoGet.cs
+++ API/DataTransferObjects/Companies/CompanyDtoGet.cs
@@ -1,4 +1,6 @@
-﻿namespace API.DataTransferObjects.Companies;
+﻿using API.Models;
+
+namespace API.DataTransferObjects.Companies;
 
 public class CompanyDtoGet
 {
@@ -6,4 +8,28 @@ public class CompanyDtoGet
     public string CompanyName { get; set; }
     public string Description { get; set; }
     public string Address { get; set; }
+
+    // implicit operator
+    public static implicit operator Company(CompanyDtoGet companyDtoGet)
+    {
+        return new Company
+        {
+            Guid = companyDtoGet.Guid,
+            CompanyName = companyDtoGet.CompanyName,
+            Description = companyDtoGet.Description,
+            Address = companyDtoGet.Address
+        };
+    }
+
+    // explicit operator
+    public static explicit operator CompanyDtoGet(Company company)
+    {
+        return new CompanyDtoGet
+        {
+            Guid = company.Guid,
+            CompanyName = company.CompanyName,
+            Description = company.Description,
+            Address = company.Address
+        };
+    }
 }
