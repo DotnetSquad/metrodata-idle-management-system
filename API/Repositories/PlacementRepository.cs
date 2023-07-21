0e837c50220153582387520c35d16834a50549fe
putuagra <iputuagra@gmail.com>
2023-07-21 14:43:40 +0700

    feat: create placement repository (#84)

---
+++ API/Repositories/PlacementRepository.cs
@@ -0,0 +1,12 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class PlacementRepository : BaseRepository<Placement>, IPlacementRepository
+{
+    public PlacementRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
