b05fbc98b68b1fcd3b6f9684b44cb06631f15d0e
Rifky Maulana <rifkymaulanaindonesia@gmail.com>
2023-07-21 14:27:33 +0700

    feat: create account repository (#76)
    * feat: create account repository
    
    * feat: add interface in AccountRepository.cs

---
+++ API/Repositories/AccountRepository.cs
@@ -0,0 +1,12 @@
+using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class AccountRepository : BaseRepository<Account>, IAccountRepository
+{
+    public AccountRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
