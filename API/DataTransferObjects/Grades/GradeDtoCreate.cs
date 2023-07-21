1ef27c094289d643fb4d011060d1d9ddc0c48bfe
aefshaeful <105156900+aefshaeful@users.noreply.github.com>
2023-07-21 17:22:57 +0700

    feat: create grade data transfer object (#126)
    * feat: create grade data transfer object
    
    * refactor: create grade data transfer object

---
+++ API/DataTransferObjects/Grades/GradeDtoCreate.cs
@@ -0,0 +1,13 @@
+﻿using API.Utilities.Enums;
+
+namespace API.DataTransferObjects.Grades;
+
+public class GradeDtoCreate
+{
+    public GradeEnum GradeLevel { get; set; }
+    public int ScoreSegment1 { get; set; }
+    public int ScoreSegment2 { get; set; }
+    public int ScoreSegment3 { get; set; }
+    public int ScoreSegment4 { get; set; }
+    public double TotalScore { get; set; }
+}
---
+++ API/DataTransferObjects/Grades/GradeDtoUpdate.cs
@@ -0,0 +1,14 @@
+﻿using API.Utilities.Enums;
+
+namespace API.DataTransferObjects.Grades;
+
+public class GradeDtoUpdate
+{
+    public Guid Guid { get; set; }
+    public GradeEnum GradeLevel { get; set; }
+    public int ScoreSegment1 { get; set; }
+    public int ScoreSegment2 { get; set; }
+    public int ScoreSegment3 { get; set; }
+    public int ScoreSegment4 { get; set; }
+    public double TotalScore { get; set; }
+}
