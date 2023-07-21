e9ad67c073a80265e5795b6077a4f1127abefd7d
putuagra <iputuagra@gmail.com>
2023-07-21 21:53:27 +0700

    feat: create implicit explicit placement dtos (#142)

--- API/DataTransferObjects/Placements/PlacementDtoUpdate.cs
+++ API/DataTransferObjects/Placements/PlacementDtoUpdate.cs
@@ -1,4 +1,6 @@
-﻿namespace API.DataTransferObjects.Placements;
+﻿using API.Models;
+
+namespace API.DataTransferObjects.Placements;
 
 public class PlacementDtoUpdate
 {
@@ -7,4 +9,30 @@ public class PlacementDtoUpdate
     public string Description { get; set; }
     public Guid EmployeeGuid { get; set; }
     public Guid CompanyGuid { get; set; }
+
+    // implicit operator
+    public static implicit operator Placement(PlacementDtoUpdate placementDtoUpdate)
+    {
+        return new Placement
+        {
+            Guid = placementDtoUpdate.Guid,
+            Title = placementDtoUpdate.Title,
+            Description = placementDtoUpdate.Description,
+            EmployeeGuid = placementDtoUpdate.EmployeeGuid,
+            CompanyGuid = placementDtoUpdate.CompanyGuid
+        };
+    }
+
+    // explicit operator
+    public static explicit operator PlacementDtoUpdate(Placement placement)
+    {
+        return new PlacementDtoUpdate
+        {
+            Guid = placement.Guid,
+            Title = placement.Title,
+            Description = placement.Description,
+            EmployeeGuid = placement.EmployeeGuid,
+            CompanyGuid = placement.CompanyGuid
+        };
+    }
 }
