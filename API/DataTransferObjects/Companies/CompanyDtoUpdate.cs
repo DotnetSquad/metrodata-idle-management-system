d23cd10aa7be26a62d8ea686e8da81538077be1d
aefshaeful <aefsaefulrohma042@gmail.com>
2023-07-21 17:01:04 +0700

    feat: create company data transfer object

---
+++ API/DataTransferObjects/Companies/CompanyDtoUpdate.cs
@@ -0,0 +1,9 @@
+﻿namespace API.DataTransferObjects.Companies;
+
+public class CompanyDtoUpdate
+{
+    public Guid Guid { get; set; }
+    public string CompanyName { get; set; }
+    public string Description { get; set; }
+    public string Address { get; set; }
+}
