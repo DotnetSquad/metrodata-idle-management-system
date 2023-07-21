dcdc357f97627ce27af5fb352f31d05eac35cc42
putuagra <iputuagra@gmail.com>
2023-07-21 16:55:24 +0700

    feat: create profile dtos (#119)

---
+++ API/DataTransferObjects/Projects/ProjectDtoGet.cs
@@ -0,0 +1,9 @@
+﻿namespace API.DataTransferObjects.Projects;
+
+public class ProjectDtoGet
+{
+    public Guid Guid { get; set; }
+    public string NameProject { get; set; }
+    public string ProjectLead { get; set; }
+    public string Description { get; set; }
+}
