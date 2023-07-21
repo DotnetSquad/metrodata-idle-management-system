8606f2082955ae0c4460c1aca784296d9f1b5a85
aefshaeful <105156900+aefshaeful@users.noreply.github.com>
2023-07-21 14:55:05 +0700

    feat: create employee project repository (#90)

---
+++ API/Repositories/EmployeeProjectRepository.cs
@@ -0,0 +1,10 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class EmployeeProjectRepository : BaseRepository<EmployeeProject>, IEmployeeProjectRepository
+{
+    public EmployeeProjectRepository(ApplicationDbContext Context) : base(Context) { }
+}
