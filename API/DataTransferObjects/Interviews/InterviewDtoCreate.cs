2002b7c31e9eb0a85af29e45399ff05c35659aaa
rifkymaulana <rifkymaulanaindonesia@gmail.com>
2023-07-21 22:29:47 +0700

    feat: create implicit explicit in interview dtos

--- API/DataTransferObjects/Interviews/InterviewDtoCreate.cs
+++ API/DataTransferObjects/Interviews/InterviewDtoCreate.cs
@@ -1,4 +1,5 @@
-﻿using API.Utilities.Enums;
+﻿using API.Models;
+using API.Utilities.Enums;
 
 namespace API.DataTransferObjects.Interviews;
 
@@ -10,4 +11,32 @@ public class InterviewDtoCreate
     public string Description { get; set; }
     public StatusInterviewEnum? StatusInterview { get; set; }
     public Guid JobGuid { get; set; }
+    
+    // implicit operator
+    public static implicit operator Interview(InterviewDtoCreate interviewDtoCreate)
+    {
+        return new Interview
+        {
+            JobName = interviewDtoCreate.JobName,
+            Link = interviewDtoCreate.Link,
+            InterviewDate = interviewDtoCreate.InterviewDate,
+            Description = interviewDtoCreate.Description,
+            StatusInterview = interviewDtoCreate.StatusInterview,
+            JobGuid = interviewDtoCreate.JobGuid
+        };
+    }
+    
+    // explicit operator
+    public static explicit operator InterviewDtoCreate(Interview interview)
+    {
+        return new InterviewDtoCreate
+        {
+            JobName = interview.JobName,
+            Link = interview.Link,
+            InterviewDate = interview.InterviewDate,
+            Description = interview.Description,
+            StatusInterview = interview.StatusInterview,
+            JobGuid = interview.JobGuid
+        };
+    }
 }
