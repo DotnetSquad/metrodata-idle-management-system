a875011e732a762f26b0dafa2e2bed1f55558e34
putuagra <iputuagra@gmail.com>
2023-07-21 11:01:35 +0700

    feat: create application db context (#68)

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,16 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Add Application Db Context
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
