1d2d11056c04951584606ba7555b87ff262e3da3
putuagra <iputuagra@gmail.com>
2023-07-21 22:07:30 +0700

    feat: create implicit explicit project dtos (#148)

--- API/DataTransferObjects/Projects/ProjectDtoGet.cs
+++ API/DataTransferObjects/Projects/ProjectDtoGet.cs
@@ -1,4 +1,6 @@
-﻿namespace API.DataTransferObjects.Projects;
+﻿using API.Models;
+
+namespace API.DataTransferObjects.Projects;
 
 public class ProjectDtoGet
 {
@@ -6,4 +8,28 @@ public class ProjectDtoGet
     public string NameProject { get; set; }
     public string ProjectLead { get; set; }
     public string Description { get; set; }
+
+    // implicit operator
+    public static implicit operator Project(ProjectDtoGet projectDtoGet)
+    {
+        return new Project
+        {
+            Guid = projectDtoGet.Guid,
+            NameProject = projectDtoGet.NameProject,
+            ProjectLead = projectDtoGet.ProjectLead,
+            Description = projectDtoGet.Description
+        };
+    }
+
+    // explicit operator
+    public static explicit operator ProjectDtoGet(Project project)
+    {
+        return new ProjectDtoGet
+        {
+            Guid = project.Guid,
+            NameProject = project.NameProject,
+            ProjectLead = project.ProjectLead,
+            Description = project.Description
+        };
+    }
 }
--- API/DataTransferObjects/Projects/ProjectDtoUpdate.cs
+++ API/DataTransferObjects/Projects/ProjectDtoUpdate.cs
@@ -1,4 +1,6 @@
-﻿namespace API.DataTransferObjects.Projects;
+﻿using API.Models;
+
+namespace API.DataTransferObjects.Projects;
 
 public class ProjectDtoUpdate
 {
@@ -6,4 +8,28 @@ public class ProjectDtoUpdate
     public string NameProject { get; set; }
     public string ProjectLead { get; set; }
     public string Description { get; set; }
+
+    // implicit operator
+    public static implicit operator Project(ProjectDtoUpdate projectDtoUpdate)
+    {
+        return new Project
+        {
+            Guid = projectDtoUpdate.Guid,
+            NameProject = projectDtoUpdate.NameProject,
+            ProjectLead = projectDtoUpdate.ProjectLead,
+            Description = projectDtoUpdate.Description
+        };
+    }
+
+    // explicit operator
+    public static explicit operator ProjectDtoUpdate(Project project)
+    {
+        return new ProjectDtoUpdate
+        {
+            Guid = project.Guid,
+            NameProject = project.NameProject,
+            ProjectLead = project.ProjectLead,
+            Description = project.Description
+        };
+    }
 }
