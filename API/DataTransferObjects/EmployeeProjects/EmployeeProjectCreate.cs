4aa675b58c34076026857713e5bf6545749803d0
Rifky Maulana <rifkymaulanaindonesia@gmail.com>
2023-07-21 21:55:22 +0700

    feat: create implicit and explicit in employee project dtos (#144)

--- API/DataTransferObjects/EmployeeProjects/EmployeeProjectCreate.cs
+++ API/DataTransferObjects/EmployeeProjects/EmployeeProjectCreate.cs
@@ -1,7 +1,29 @@
+using API.Models;
+
 namespace API.DataTransferObjects.EmployeeProjects;
 
 public class EmployeeProjectCreate
 {
     public Guid EmployeeGuid { get; set; }
     public Guid ProjectGuid { get; set; }
+
+    // implicit operator
+    public static implicit operator EmployeeProject(EmployeeProjectCreate employeeProjectCreate)
+    {
+        return new EmployeeProject()
+        {
+            EmployeeGuid = employeeProjectCreate.EmployeeGuid,
+            ProjectGuid = employeeProjectCreate.ProjectGuid
+        };
+    }
+
+    // explicit operator
+    public static explicit operator EmployeeProjectCreate(EmployeeProject employeeProject)
+    {
+        return new EmployeeProjectCreate()
+        {
+            EmployeeGuid = employeeProject.EmployeeGuid,
+            ProjectGuid = employeeProject.ProjectGuid
+        };
+    }
 }
--- API/DataTransferObjects/EmployeeProjects/EmployeeProjectGet.cs
+++ API/DataTransferObjects/EmployeeProjects/EmployeeProjectGet.cs
@@ -1,3 +1,5 @@
+using API.Models;
+
 namespace API.DataTransferObjects.EmployeeProjects;
 
 public class EmployeeProjectGet
@@ -5,4 +7,26 @@ public class EmployeeProjectGet
     public Guid Guid { get; set; }
     public Guid EmployeeGuid { get; set; }
     public Guid ProjectGuid { get; set; }
+    
+    // implicit operator
+    public static implicit operator EmployeeProject(EmployeeProjectGet employeeProjectGet)
+    {
+        return new EmployeeProject()
+        {
+            Guid = employeeProjectGet.Guid,
+            EmployeeGuid = employeeProjectGet.EmployeeGuid,
+            ProjectGuid = employeeProjectGet.ProjectGuid
+        };
+    }
+    
+    // explicit operator
+    public static explicit operator EmployeeProjectGet(EmployeeProject employeeProject)
+    {
+        return new EmployeeProjectGet()
+        {
+            Guid = employeeProject.Guid,
+            EmployeeGuid = employeeProject.EmployeeGuid,
+            ProjectGuid = employeeProject.ProjectGuid
+        };
+    }
 }
