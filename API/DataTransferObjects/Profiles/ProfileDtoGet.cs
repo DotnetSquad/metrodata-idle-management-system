b6491b29774d9a77f76c2cba57050a7329d6b7df
putuagra <iputuagra@gmail.com>
2023-07-21 16:49:05 +0700

    feat: createprofile dtos

---
+++ API/DataTransferObjects/Profiles/ProfileDtoGet.cs
@@ -0,0 +1,9 @@
+﻿namespace API.DataTransferObjects.Profiles;
+
+public class ProfileDtoGet
+{
+    public Guid Guid { get; set; }
+    public string Skills { get; set; }
+    public string Linkedin { get; set; }
+    public string Resume { get; set; }
+}
