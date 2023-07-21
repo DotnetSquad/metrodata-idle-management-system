9eda1931a7524644a66322b63a8d166472d0cce8
aefshaeful <aefsaefulrohma042@gmail.com>
2023-07-21 22:24:58 +0700

    feat: create implicit explicit job dtos

--- API/DataTransferObjects/Jobs/JobDtoCreate.cs
+++ API/DataTransferObjects/Jobs/JobDtoCreate.cs
@@ -1,3 +1,5 @@
+using API.Models;
+
 namespace API.DataTransferObjects.Jobs;
 
 public class JobDtoCreate
@@ -5,4 +7,26 @@ public class JobDtoCreate
     public string JobName { get; set; }
     public string Description { get; set; }
     public Guid CompanyGuid { get; set; }
+
+    // implicit operator
+    public static implicit operator Job(JobDtoCreate jobDtoCreate)
+    {
+        return new Job
+        {
+            JobName = jobDtoCreate.JobName,
+            Description = jobDtoCreate.Description,
+            CompanyGuid = jobDtoCreate.CompanyGuid
+        };
+    }
+
+    // explicit operator
+    public static explicit operator JobDtoCreate(Job job)
+    {
+        return new JobDtoCreate
+        {
+            JobName = job.JobName,
+            Description = job.Description,
+            CompanyGuid = job.CompanyGuid
+        };
+    }
 }
--- API/DataTransferObjects/Jobs/JobDtoUpdate.cs
+++ API/DataTransferObjects/Jobs/JobDtoUpdate.cs
@@ -1,3 +1,5 @@
+using API.Models;
+
 namespace API.DataTransferObjects.Jobs;
 
 public class JobDtoUpdate
@@ -6,4 +8,28 @@ public class JobDtoUpdate
     public string JobName { get; set; }
     public string Description { get; set; }
     public Guid CompanyGuid { get; set; }
+
+    // implicit operator
+    public static implicit operator Job(JobDtoUpdate jobDtoUpdate)
+    {
+        return new Job
+        {
+            Guid = jobDtoUpdate.Guid,
+            JobName = jobDtoUpdate.JobName,
+            Description = jobDtoUpdate.Description,
+            CompanyGuid = jobDtoUpdate.CompanyGuid
+        };
+    }
+
+    // explicit operator
+    public static explicit operator JobDtoUpdate(Job job)
+    {
+        return new JobDtoUpdate
+        {
+            Guid = job.Guid,
+            JobName = job.JobName,
+            Description = job.Description,
+            CompanyGuid = job.CompanyGuid
+        };
+    }
 }
