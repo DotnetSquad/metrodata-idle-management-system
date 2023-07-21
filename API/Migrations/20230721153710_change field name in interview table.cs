7a18f29380e69d41d15bcdeefb58fa5ca1770a36
Rifky Maulana <rifkymaulanaindonesia@gmail.com>
2023-07-21 22:45:11 +0700

    refactor: rename field job name in interview table (#158)
    * refactor: rename field job_name in interview tabel
    
    * feat: add migration files

---
+++ API/Migrations/20230721153710_change field name in interview table.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class changefieldnameininterviewtable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "job_name",
+                table: "tb_m_interviews",
+                newName: "title");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "title",
+                table: "tb_m_interviews",
+                newName: "job_name");
+        }
+    }
+}
