86c2dd25a5b9242b174ec1b82eb8495022bda87e
rifkymaulana <rifkymaulanaindonesia@gmail.com>
2023-07-21 20:17:52 +0700

    feat: create explicit and implicit in role dtos

--- API/DataTransferObjects/Roles/RoleDtoCreate.cs
+++ API/DataTransferObjects/Roles/RoleDtoCreate.cs
@@ -3,4 +3,20 @@ namespace API.DataTransferObjects.Role;
 public class RoleDtoCreate
 {
     public string Name { get; set; }
+
+    public static implicit operator RoleDtoGet(RoleDtoCreate roleDtoCreate)
+    {
+        return new()
+        {
+            Name = roleDtoCreate.Name
+        };
+    }
+
+    public static explicit operator RoleDtoUpdate(RoleDtoCreate roleDtoCreate)
+    {
+        return new()
+        {
+            Name = roleDtoCreate.Name
+        };
+    }
 }
