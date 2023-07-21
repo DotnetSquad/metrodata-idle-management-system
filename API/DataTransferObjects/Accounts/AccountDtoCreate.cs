f2ddda83db318ce6fa280df94893122b51ae8ab2
putuagra <iputuagra@gmail.com>
2023-07-21 17:14:54 +0700

    chore: add nullable data dtos (#125)

--- API/DataTransferObjects/Accounts/AccountDtoCreate.cs
+++ API/DataTransferObjects/Accounts/AccountDtoCreate.cs
@@ -4,7 +4,7 @@ public class AccountDtoCreate
 {
     public string Password { get; set; }
     public bool IsDeleted { get; set; }
-    public int Otp { get; set; }
-    public bool IsUsed { get; set; }
-    public DateTime ExpiredTime { get; set; }
+    public int? Otp { get; set; }
+    public bool? IsUsed { get; set; }
+    public DateTime? ExpiredTime { get; set; }
 }
--- API/DataTransferObjects/Accounts/AccountDtoGet.cs
+++ API/DataTransferObjects/Accounts/AccountDtoGet.cs
@@ -5,7 +5,7 @@ public class AccountDtoGet
     public Guid Guid { get; set; }
     public string Password { get; set; }
     public bool IsDeleted { get; set; }
-    public int Otp { get; set; }
-    public bool IsUsed { get; set; }
-    public DateTime ExpiredTime { get; set; }
+    public int? Otp { get; set; }
+    public bool? IsUsed { get; set; }
+    public DateTime? ExpiredTime { get; set; }
 }
