dd6867c75c44e9e74697f7a39eeecbc6aee70c15
putuagra <iputuagra@gmail.com>
2023-07-21 16:34:32 +0700

    feat: create interview dtos

---
+++ API/DataTransferObjects/Interviews/InterviewDtoCreate.cs
@@ -0,0 +1,13 @@
+﻿using API.Utilities.Enums;
+
+namespace API.DataTransferObjects.Interviews;
+
+public class InterviewDtoCreate
+{
+    public string JobName { get; set; }
+    public string? Link { get; set; }
+    public DateTime InterviewDate { get; set; }
+    public string Description { get; set; }
+    public StatusInterviewEnum? StatusInterview { get; set; }
+    public Guid JobGuid { get; set; }
+}
