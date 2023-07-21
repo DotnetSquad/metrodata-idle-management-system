2d531f66f724307b1e16f99f91fcf958376782c4
rifkymaulana <rifkymaulanaindonesia@gmail.com>
2023-07-21 21:20:15 +0700

    feat: create implicit explicit employee dtos

--- API/DataTransferObjects/Employees/EmployeeDtoCreate.cs
+++ API/DataTransferObjects/Employees/EmployeeDtoCreate.cs
@@ -1,3 +1,4 @@
+using API.Models;
 using API.Utilities.Enums;
 
 namespace API.DataTransferObjects.Employees;
@@ -15,4 +16,42 @@ public class EmployeeDtoCreate
     public StatusEnum Status { get; set; }
     public Guid? GradeGuid { get; set; }
     public Guid? ProfileGuid { get; set; }
+    
+    // implicit operator
+    public static implicit operator Employee(EmployeeDtoCreate employeeDtoCreate)
+    {
+        return new Employee()
+        {
+            Nik = employeeDtoCreate.Nik,
+            FirstName = employeeDtoCreate.FirstName,
+            LastName = employeeDtoCreate.LastName,
+            BirthDate = employeeDtoCreate.BirthDate,
+            Gender = employeeDtoCreate.Gender,
+            HiringDate = employeeDtoCreate.HiringDate,
+            Email = employeeDtoCreate.Email,
+            PhoneNumber = employeeDtoCreate.PhoneNumber,
+            Status = employeeDtoCreate.Status,
+            GradeGuid = employeeDtoCreate.GradeGuid,
+            ProfileGuid = employeeDtoCreate.ProfileGuid
+        };
+    }
+    
+    // explicit operator
+    public static explicit operator EmployeeDtoCreate(Employee employee)
+    {
+        return new EmployeeDtoCreate()
+        {
+            Nik = employee.Nik,
+            FirstName = employee.FirstName,
+            LastName = employee.LastName,
+            BirthDate = employee.BirthDate,
+            Gender = employee.Gender,
+            HiringDate = employee.HiringDate,
+            Email = employee.Email,
+            PhoneNumber = employee.PhoneNumber,
+            Status = employee.Status,
+            GradeGuid = employee.GradeGuid,
+            ProfileGuid = employee.ProfileGuid
+        };
+    }
 }
