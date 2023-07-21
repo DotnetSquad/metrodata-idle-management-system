17278251ddd2d51906d224b7410dfe33566e7dfb
putuagra <iputuagra@gmail.com>
2023-07-21 14:54:32 +0700

    feat: create job repository (#89)

---
+++ API/Repositories/JobRepository.cs
@@ -0,0 +1,12 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class JobRepository : BaseRepository<Job>, IJobInterface
+{
+    public JobRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
