83047a87b5be6e237fa49e0bbaed4780c39f9cbf
rifkymaulana <rifkymaulanaindonesia@gmail.com>
2023-07-21 16:40:44 +0700

    feat: add Guid in create dto

--- API/DataTransferObjects/Jobs/JobDtoCreate.cs
+++ API/DataTransferObjects/Jobs/JobDtoCreate.cs
@@ -2,7 +2,6 @@ namespace API.DataTransferObjects.Jobs;
 
 public class JobDtoCreate
 {
-    public Guid Guid { get; set; }
     public string JobName { get; set; }
     public string Description { get; set; }
     public Guid CompanyGuid { get; set; }
--- API/DataTransferObjects/Jobs/JobDtoGet.cs
+++ API/DataTransferObjects/Jobs/JobDtoGet.cs
@@ -2,6 +2,7 @@ namespace API.DataTransferObjects.Jobs;
 
 public class JobDtoGet
 {
+    public Guid Guid { get; set; }
     public string JobName { get; set; }
     public string Description { get; set; }
     public Guid CompanyGuid { get; set; }
