0512dfc4205d73414457fa99f00049a50ccb2ca6
aefshaeful <105156900+aefshaeful@users.noreply.github.com>
2023-07-21 22:26:38 +0700

    feat: create implicit explicit job dtos (#155)

--- API/DataTransferObjects/Jobs/JobDtoGet.cs
+++ API/DataTransferObjects/Jobs/JobDtoGet.cs
@@ -1,3 +1,6 @@
+using API.DataTransferObjects.Accounts;
+using API.Models;
+
 namespace API.DataTransferObjects.Jobs;
 
 public class JobDtoGet
@@ -6,4 +9,28 @@ public class JobDtoGet
     public string JobName { get; set; }
     public string Description { get; set; }
     public Guid CompanyGuid { get; set; }
+
+    // implicit operator
+    public static implicit operator Job(JobDtoGet jobDtoGet)
+    {
+        return new Job
+        {
+            Guid = jobDtoGet.Guid,
+            JobName = jobDtoGet.JobName,
+            Description = jobDtoGet.Description,
+            CompanyGuid = jobDtoGet.CompanyGuid
+        };
+    }
+
+    // explicit operator
+    public static explicit operator JobDtoGet(Job job)
+    {
+        return new JobDtoGet
+        {
+            Guid = job.Guid,
+            JobName = job.JobName,
+            Description = job.Description,
+            CompanyGuid = job.CompanyGuid
+        };
+    }
 }
