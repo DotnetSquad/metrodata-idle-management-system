561d03aab3c66d4438cd011a1deaf8da57f92e16
aefshaeful <105156900+aefshaeful@users.noreply.github.com>
2023-07-21 14:42:13 +0700

    feat: create grade repository (#83)

---
+++ API/Repositories/GradeRepository.cs
@@ -0,0 +1,10 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class GradeRepository : BaseRepository<Grade>, IGradeRepository
+{
+    public GradeRepository(ApplicationDbContext Context) : base(Context) { }
+}
