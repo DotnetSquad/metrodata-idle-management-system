dfdcc120ebdeb32fd49e09aa1aafc0db1466dd05
putuagra <iputuagra@gmail.com>
2023-07-21 22:05:29 +0700

    feat: create implicit explicit project dtos

--- API/DataTransferObjects/Projects/ProjectDtoCreate.cs
+++ API/DataTransferObjects/Projects/ProjectDtoCreate.cs
@@ -1,8 +1,32 @@
-﻿namespace API.DataTransferObjects.Projects;
+﻿using API.Models;
+
+namespace API.DataTransferObjects.Projects;
 
 public class ProjectDtoCreate
 {
     public string NameProject { get; set; }
     public string ProjectLead { get; set; }
     public string Description { get; set; }
+
+    // implicit operator
+    public static implicit operator Project(ProjectDtoCreate projectDtoCreate)
+    {
+        return new Project
+        {
+            NameProject = projectDtoCreate.NameProject,
+            ProjectLead = projectDtoCreate.ProjectLead,
+            Description = projectDtoCreate.Description
+        };
+    }
+
+    // explicit operator
+    public static explicit operator ProjectDtoCreate(Project project)
+    {
+        return new ProjectDtoCreate
+        {
+            NameProject = project.NameProject,
+            ProjectLead = project.ProjectLead,
+            Description = project.Description
+        };
+    }
 }
