3e0322a2d0baef9a65cff9b0e9d3a3b5036cc42d
aefshaeful <aefsaefulrohma042@gmail.com>
2023-07-21 15:01:39 +0700

    feat: create project repository

---
+++ API/Repositories/ProjectRepository.cs
@@ -0,0 +1,10 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class ProjectRepository : BaseRepository<Project>, IProjectRepository
+{
+    public ProjectRepository(ApplicationDbContext context) : base(context) { }
+}
