a95fbf09db0074c572e8824bde4d1996a7740936
putuagra <iputuagra@gmail.com>
2023-07-21 15:03:39 +0700

    feat: create interview repository (#93)

---
+++ API/Repositories/InterviewRepository.cs
@@ -0,0 +1,12 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class InterviewRepository : BaseRepository<Interview>, IInterviewRepository
+{
+    public InterviewRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
