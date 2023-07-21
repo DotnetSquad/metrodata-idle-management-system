076daaa4324f15f6fa63abd2fb3b96f35248048a
putuagra <iputuagra@gmail.com>
2023-07-21 21:51:06 +0700

    feat: create implicit explicit placement dtos

--- API/DataTransferObjects/Placements/PlacementDtoCreate.cs
+++ API/DataTransferObjects/Placements/PlacementDtoCreate.cs
@@ -1,4 +1,6 @@
-﻿namespace API.DataTransferObjects.Placements;
+﻿using API.Models;
+
+namespace API.DataTransferObjects.Placements;
 
 public class PlacementDtoCreate
 {
@@ -6,4 +8,28 @@ public class PlacementDtoCreate
     public string Description { get; set; }
     public Guid EmployeeGuid { get; set; }
     public Guid CompanyGuid { get; set; }
+
+    // implicit operator
+    public static implicit operator Placement(PlacementDtoCreate placementDtoCreate)
+    {
+        return new Placement
+        {
+            Title = placementDtoCreate.Title,
+            Description = placementDtoCreate.Description,
+            EmployeeGuid = placementDtoCreate.EmployeeGuid,
+            CompanyGuid = placementDtoCreate.CompanyGuid
+        };
+    }
+
+    // explicit operator
+    public static explicit operator PlacementDtoCreate(Placement placement)
+    {
+        return new PlacementDtoCreate
+        {
+            Title = placement.Title,
+            Description = placement.Description,
+            EmployeeGuid = placement.EmployeeGuid,
+            CompanyGuid = placement.CompanyGuid
+        };
+    }
 }
--- API/DataTransferObjects/Placements/PlacementDtoGet.cs
+++ API/DataTransferObjects/Placements/PlacementDtoGet.cs
@@ -1,4 +1,6 @@
-﻿namespace API.DataTransferObjects.Placements;
+﻿using API.Models;
+
+namespace API.DataTransferObjects.Placements;
 
 public class PlacementDtoGet
 {
@@ -7,4 +9,30 @@ public class PlacementDtoGet
     public string Description { get; set; }
     public Guid EmployeeGuid { get; set; }
     public Guid CompanyGuid { get; set; }
+
+    // implicit operator
+    public static implicit operator Placement(PlacementDtoGet placementDtoGet)
+    {
+        return new Placement
+        {
+            Guid = placementDtoGet.Guid,
+            Title = placementDtoGet.Title,
+            Description = placementDtoGet.Description,
+            EmployeeGuid = placementDtoGet.EmployeeGuid,
+            CompanyGuid = placementDtoGet.CompanyGuid
+        };
+    }
+
+    // explicit operator
+    public static explicit operator PlacementDtoGet(Placement placement)
+    {
+        return new PlacementDtoGet
+        {
+            Guid = placement.Guid,
+            Title = placement.Title,
+            Description = placement.Description,
+            EmployeeGuid = placement.EmployeeGuid,
+            CompanyGuid = placement.CompanyGuid
+        };
+    }
 }
