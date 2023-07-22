12738f2b5a543732a78140becbd9cd9faf97877e
rifkymaulana <rifkymaulanaindonesia@gmail.com>
2023-07-22 20:46:20 +0700

    feat: CRUD for role

--- API/Program.cs
+++ API/Program.cs
@@ -1,15 +1,23 @@
+using API.Contracts;
 using API.Data;
+using API.Repositories;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 
 // Add Application Db Context
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+// Add Repositories
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+
+// Add Services
+builder.Services.AddScoped<RoleService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
