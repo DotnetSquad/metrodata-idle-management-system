2766af1453b136d77c403ec5010f71e4a8b2d8f2
aefshaeful <aefsaefulrohma042@gmail.com>
2023-07-24 14:56:56 +0700

    feat: create fluent validations employee interview dtos

---
+++ API/Utilities/Validations/EmployeeInterviews/EmployeeInterviewValidatorCreate.cs
@@ -0,0 +1,16 @@
+﻿using API.DataTransferObjects.EmployeeInterviews;
+using FluentValidation;
+
+namespace API.Utilities.Validations.EmployeeInterviews;
+
+public class EmployeeInterviewValidatorCreate : AbstractValidator<EmployeeInterviewDtoCreate>
+{
+    public EmployeeInterviewValidatorCreate()
+    {
+        RuleFor(x => x.EmployeeGuid)
+            .NotEmpty();
+
+        RuleFor(x => x.InterviewGuid)
+            .NotEmpty();
+    }
+}
---
+++ API/Utilities/Validations/EmployeeInterviews/EmployeeInterviewValidatorUpdate.cs
@@ -0,0 +1,19 @@
+﻿using API.DataTransferObjects.EmployeeInterviews;
+using FluentValidation;
+
+namespace API.Utilities.Validations.EmployeeInterviews;
+
+public class EmployeeInterviewValidatorUpdate : AbstractValidator<EmployeeInterviewDtoUpdate>
+{
+    public EmployeeInterviewValidatorUpdate()
+    {
+        RuleFor(x => x.Guid)
+           .NotEmpty();
+
+        RuleFor(x => x.EmployeeGuid)
+           .NotEmpty();
+
+        RuleFor(x => x.InterviewGuid)
+           .NotEmpty();
+    }
+}
