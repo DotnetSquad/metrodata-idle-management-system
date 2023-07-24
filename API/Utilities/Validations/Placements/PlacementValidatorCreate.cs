b30618f500bfc1a8039dd9352fdced861d8248e6
putuagra <iputuagra@gmail.com>
2023-07-24 11:43:35 +0700

    feat: create fluent validation placement dtos (#208)

---
+++ API/Utilities/Validations/Placements/PlacementValidatorCreate.cs
@@ -0,0 +1,18 @@
+﻿using API.DataTransferObjects.Placements;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Placements;
+
+public class PlacementValidatorCreate : AbstractValidator<PlacementDtoCreate>
+{
+    public PlacementValidatorCreate()
+    {
+        RuleFor(x => x.Title).NotEmpty();
+
+        RuleFor(x => x.Description).NotEmpty();
+
+        RuleFor(x => x.EmployeeGuid).NotEmpty();
+
+        RuleFor(x => x.CompanyGuid).NotEmpty();
+    }
+}
