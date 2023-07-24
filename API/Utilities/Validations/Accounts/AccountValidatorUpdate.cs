c2cececf1cfd10b029169d4365d62cf1bf9dc008
putuagra <iputuagra@gmail.com>
2023-07-24 10:47:31 +0700

    feat: create fluent validations account dtos

---
+++ API/Utilities/Validations/Accounts/AccountValidatorUpdate.cs
@@ -0,0 +1,19 @@
+﻿using API.DataTransferObjects.Accounts;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Accounts;
+
+public class AccountValidatorUpdate : AbstractValidator<AccountDtoUpdate>
+{
+    public AccountValidatorUpdate()
+    {
+        RuleFor(p => p.Guid).NotEmpty();
+
+        RuleFor(p => p.Password).NotEmpty().WithMessage("Password is required").MinimumLength(8).WithMessage("Password must be at least 8 characters long").Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+           .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+           .Matches("[0-9]").WithMessage("Password must contain at least one digit.")
+           .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character.");
+
+        RuleFor(p => p.IsDeleted).NotEmpty();
+    }
+}
