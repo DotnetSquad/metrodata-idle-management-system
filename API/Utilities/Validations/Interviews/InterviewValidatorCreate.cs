fb596f99228c90f1413e1768f06d2cde33f808e2
putuagra <iputuagra@gmail.com>
2023-07-24 14:29:57 +0700

    feat: create fluent validation interview dtos (#221)

---
+++ API/Utilities/Validations/Interviews/InterviewValidatorCreate.cs
@@ -0,0 +1,20 @@
+﻿using API.DataTransferObjects.Interviews;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Interviews;
+
+public class InterviewValidatorCreate : AbstractValidator<InterviewDtoCreate>
+{
+    public InterviewValidatorCreate()
+    {
+        RuleFor(x => x.Title).NotEmpty();
+
+        RuleFor(x => x.InterviewDate).NotEmpty();
+
+        RuleFor(x => x.Description).NotEmpty();
+
+        RuleFor(x => x.StatusInterview).NotNull().IsInEnum().WithMessage("Status Interview must be either 0 is Pending status, 1 is Accepted status, and 2 is Rejected");
+
+        RuleFor(x => x.JobGuid).NotEmpty();
+    }
+}
---
+++ API/Utilities/Validations/Interviews/InterviewValidatorUpdate.cs
@@ -0,0 +1,22 @@
+﻿using API.DataTransferObjects.Interviews;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Interviews;
+
+public class InterviewValidatorUpdate : AbstractValidator<InterviewDtoUpdate>
+{
+    public InterviewValidatorUpdate()
+    {
+        RuleFor(x => x.Guid).NotEmpty();
+
+        RuleFor(x => x.Title).NotEmpty();
+
+        RuleFor(x => x.InterviewDate).NotEmpty();
+
+        RuleFor(x => x.Description).NotEmpty();
+
+        RuleFor(x => x.StatusInterview).NotNull().IsInEnum().WithMessage("Status Interview must be either 0 is Pending status, 1 is Accepted status, and 2 is Rejected");
+
+        RuleFor(x => x.JobGuid).NotEmpty();
+    }
+}
