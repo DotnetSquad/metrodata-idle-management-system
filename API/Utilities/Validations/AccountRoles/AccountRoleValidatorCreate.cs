c3ebdf92645e96a927337351da155e096048521a
aefshaeful <105156900+aefshaeful@users.noreply.github.com>
2023-07-24 10:59:39 +0700

    feat: create fluent validations account role dtos (#207)

---
+++ API/Utilities/Validations/AccountRoles/AccountRoleValidatorCreate.cs
@@ -0,0 +1,16 @@
+﻿using API.DataTransferObjects.AccountRoles;
+using FluentValidation;
+
+namespace API.Utilities.Validations.AccountRoles;
+
+public class AccountRoleValidatorCreate : AbstractValidator<AccountRoleDtoCreate>
+{
+    public AccountRoleValidatorCreate()
+    {
+        RuleFor(x => x.AccountGuid)
+            .NotEmpty();
+
+        RuleFor(x => x.RoleGuid)
+           .NotEmpty();
+    }
+}
