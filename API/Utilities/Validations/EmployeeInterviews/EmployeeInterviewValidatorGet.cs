b0d526d1849863240b6bd03ce79fe4b7a339a685
aefshaeful <105156900+aefshaeful@users.noreply.github.com>
2023-07-24 14:59:00 +0700

    feat: create fluent validations employee interview dtos (#225)

---
+++ API/Utilities/Validations/EmployeeInterviews/EmployeeInterviewValidatorGet.cs
@@ -0,0 +1,19 @@
+﻿using API.DataTransferObjects.EmployeeInterviews;
+using FluentValidation;
+
+namespace API.Utilities.Validations.EmployeeInterviews;
+
+public class EmployeeInterviewValidatorGet : AbstractValidator<EmployeeInterviewDtoGet>
+{
+    public EmployeeInterviewValidatorGet()
+    {
+        RuleFor(x => x.Guid)
+            .NotEmpty();
+
+        RuleFor(x => x.EmployeeGuid)
+            .NotEmpty();
+
+        RuleFor(x => x.InterviewGuid)
+            .NotEmpty();
+    }
+}
