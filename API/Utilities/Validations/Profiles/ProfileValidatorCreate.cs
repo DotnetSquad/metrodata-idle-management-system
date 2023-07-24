7f16f8c65ac942205cf0b974fd7d68a6d546dc4d
aefshaeful <105156900+aefshaeful@users.noreply.github.com>
2023-07-24 14:24:08 +0700

    feat: create fluent validations profile dtos (#220)

---
+++ API/Utilities/Validations/Profiles/ProfileValidatorCreate.cs
@@ -0,0 +1,19 @@
+﻿using API.DataTransferObjects.Profiles;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Profiles;
+
+public class ProfileValidatorCreate : AbstractValidator<ProfileDtoCreate>
+{
+    public ProfileValidatorCreate()
+    {
+        RuleFor(x => x.Skills)
+            .NotEmpty();
+
+        RuleFor(x => x.Linkedin)
+           .NotEmpty();
+
+        RuleFor(x => x.Resume)
+           .NotEmpty();
+    }
+}
---
+++ API/Utilities/Validations/Profiles/ProfileValidatorUpdate.cs
@@ -0,0 +1,22 @@
+﻿using API.DataTransferObjects.Profiles;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Profiles;
+
+public class ProfileValidatorUpdate : AbstractValidator<ProfileDtoUpdate>
+{
+    public ProfileValidatorUpdate()
+    {
+        RuleFor(x => x.Guid)
+          .NotEmpty();
+
+        RuleFor(x => x.Skills)
+            .NotEmpty();
+
+        RuleFor(x => x.Linkedin)
+           .NotEmpty();
+
+        RuleFor(x => x.Resume)
+           .NotEmpty();
+    }
+}
