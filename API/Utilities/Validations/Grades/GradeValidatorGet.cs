07549c6df46248b4c603bec7d0d540a1c8960920
aefshaeful <105156900+aefshaeful@users.noreply.github.com>
2023-07-24 13:31:14 +0700

    feat: creat fluent validations grade dtos (#214)
    * feat: creat fluent validations grade dtos
    
    * refactor: creat fluent validations grade dtos

---
+++ API/Utilities/Validations/Grades/GradeValidatorGet.cs
@@ -0,0 +1,40 @@
+﻿using API.DataTransferObjects.Grades;
+using API.Utilities.Enums;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Grades;
+
+public class GradeValidatorGet : AbstractValidator<GradeDtoGet>
+{
+    public GradeValidatorGet()
+    {
+        RuleFor(x => x.Guid)
+            .NotEmpty();
+
+        RuleFor(x => x.GradeLevel)
+            .NotEmpty()
+            .Must(x => x == GradeEnum.A || x == GradeEnum.B)
+            .WithMessage("GradeLevel must be either 0 is grade A or 1 is grade B");
+
+        RuleFor(x => x.ScoreSegment1)
+            .NotEmpty()
+            .InclusiveBetween(0, 100)
+            .WithMessage("ScoreSegment1 must be between 0 and 100");
+
+        RuleFor(x => x.ScoreSegment2)
+            .NotEmpty()
+            .InclusiveBetween(0, 100)
+            .WithMessage("ScoreSegment2 must be between 0 and 100");
+
+        RuleFor(x => x.ScoreSegment3)
+            .NotEmpty()
+            .InclusiveBetween(0, 100)
+            .WithMessage("ScoreSegment3 must be between 0 and 100");
+
+        RuleFor(x => x.ScoreSegment4)
+            .NotEmpty()
+            .InclusiveBetween(0, 100)
+            .WithMessage("ScoreSegment4 must be between 0 and 100");
+    }
+}
+
