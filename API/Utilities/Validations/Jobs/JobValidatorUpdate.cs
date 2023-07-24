a27a0c6b13e81c7b48b0c22f9c0f4ecf11c83f92
aefshaeful <aefsaefulrohma042@gmail.com>
2023-07-24 13:55:35 +0700

    feat: create fluent validations job dtos

---
+++ API/Utilities/Validations/Jobs/JobValidatorUpdate.cs
@@ -0,0 +1,22 @@
+﻿using API.DataTransferObjects.Jobs;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Jobs;
+
+public class JobValidatorUpdate : AbstractValidator<JobDtoUpdate>
+{
+    public JobValidatorUpdate()
+    {
+        RuleFor(x => x.Guid)
+           .NotEmpty();
+
+        RuleFor(x => x.JobName)
+            .NotEmpty();
+
+        RuleFor(x => x.Description)
+           .NotEmpty();
+
+        RuleFor(x => x.CompanyGuid)
+           .NotEmpty();
+    }
+}
