b1201a56f4739ce6527cc8c5c18b8082b1962120
aefshaeful <aefsaefulrohma042@gmail.com>
2023-07-24 14:13:48 +0700

    feat: create fluent validations profile dtos

---
+++ API/Utilities/Validations/Profiles/ProfileValidatorGet.cs
@@ -0,0 +1,22 @@
+﻿using API.DataTransferObjects.Profiles;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Profiles;
+
+public class ProfileValidatorGet : AbstractValidator<ProfileDtoGet>
+{
+    public ProfileValidatorGet()
+    {
+        RuleFor(x => x.Guid)
+          .NotEmpty();
+
+        RuleFor(x => x.Skills)
+            .NotEmpty();
+
+        RuleFor(x => x.Linkedin)
+           .NotEmpty();
+
+        RuleFor(x => x.Resume)
+           .NotEmpty();
+    }
+}
