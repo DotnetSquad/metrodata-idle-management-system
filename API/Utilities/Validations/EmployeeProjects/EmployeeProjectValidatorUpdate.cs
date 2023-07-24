d57cee35bb4986975b35e03cff583efb04a55dcb
putuagra <iputuagra@gmail.com>
2023-07-24 13:49:08 +0700

    feat: create fluent validations employeeproject dtos

---
+++ API/Utilities/Validations/EmployeeProjects/EmployeeProjectValidatorUpdate.cs
@@ -0,0 +1,16 @@
+﻿using API.DataTransferObjects.EmployeeProjects;
+using FluentValidation;
+
+namespace API.Utilities.Validations.EmployeeProjects;
+
+public class EmployeeProjectValidatorUpdate : AbstractValidator<EmployeeProjectDtoUpdate>
+{
+    public EmployeeProjectValidatorUpdate()
+    {
+        RuleFor(x => x.Guid).NotEmpty();
+
+        RuleFor(x => x.EmployeeGuid).NotEmpty();
+
+        RuleFor(x => x.ProjectGuid).NotEmpty();
+    }
+}
