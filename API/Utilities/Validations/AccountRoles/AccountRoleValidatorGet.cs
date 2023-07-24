4639c882c8f64d089a6a7288a004a97a9bb8a322
aefshaeful <aefsaefulrohma042@gmail.com>
2023-07-24 10:57:29 +0700

    feat: create fluent validations account role dtos

---
+++ API/Utilities/Validations/AccountRoles/AccountRoleValidatorGet.cs
@@ -0,0 +1,19 @@
+﻿using API.DataTransferObjects.AccountRoles;
+using FluentValidation;
+
+namespace API.Utilities.Validations.AccountRoles;
+
+public class AccountRoleValidatorGet : AbstractValidator<AccountRoleDtoGet>
+{
+    public AccountRoleValidatorGet()
+    {
+        RuleFor(x => x.Guid)
+            .NotEmpty();
+
+        RuleFor(x => x.AccountGuid)
+            .NotEmpty();
+
+        RuleFor(x => x.RoleGuid)
+           .NotEmpty();
+    }
+}
---
+++ API/Utilities/Validations/AccountRoles/AccountRoleValidatorUpdate.cs
@@ -0,0 +1,19 @@
+﻿using API.DataTransferObjects.AccountRoles;
+using FluentValidation;
+
+namespace API.Utilities.Validations.AccountRoles;
+
+public class AccountRoleValidatorUpdate : AbstractValidator<AccountRoleDtoUpdate>
+{
+    public AccountRoleValidatorUpdate()
+    {
+        RuleFor(x => x.Guid)
+            .NotEmpty();
+
+        RuleFor(x => x.AccountGuid)
+            .NotEmpty();
+
+        RuleFor(x => x.RoleGuid)
+           .NotEmpty();
+    }
+}
