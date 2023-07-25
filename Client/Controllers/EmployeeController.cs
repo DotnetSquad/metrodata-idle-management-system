caa051ba1325513318801fe6c0241dae952eb251
putuagra <iputuagra@gmail.com>
2023-07-25 14:26:53 +0700

    feat: create view datatable employee

---
+++ Client/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using Client.Contracts;
+using Client.DataTransferObjects.Employees;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers;
+
+public class EmployeeController : Controller
+{
+    private readonly IEmployeeRepository _repository;
+
+    public EmployeeController(IEmployeeRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var result = await _repository.Get();
+        var ListEmployee = new List<EmployeeDtoGet>();
+
+        if (result.Data != null)
+        {
+            ListEmployee = result.Data.ToList();
+        }
+        return View(ListEmployee);
+    }
+}
---
+++ Client/Repositories/EmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿using Client.Contracts;
+using Client.DataTransferObjects.Employees;
+
+namespace Client.Repositories;
+
+public class EmployeeRepository : BaseRepository<EmployeeDtoGet, Guid>, IEmployeeRepository
+{
+    public EmployeeRepository(string request = "Employee/") : base(request)
+    {
+    }
+}
