5ef8569fbb3403fb04e79121a8facdf0bc9b5dce
aefshaeful <aefsaefulrohma042@gmail.com>
2023-07-26 14:05:16 +0700

    feat: create view create job feature

--- Client/Controllers/JobController.cs
+++ Client/Controllers/JobController.cs
@@ -1,4 +1,5 @@
 ﻿using Client.Contracts;
+using Client.DataTransferObjects.Companies;
 using Client.DataTransferObjects.Jobs;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,10 +8,11 @@ namespace Client.Controllers;
 public class JobController : Controller
 {
     private readonly IJobRepository _repository;
-
-    public JobController(IJobRepository repository)
+    private readonly ICompanyRepository _companyRepository;
+    public JobController(IJobRepository repository, ICompanyRepository companyRepository)
     {
         _repository = repository;
+        _companyRepository = companyRepository;
     }
 
     [HttpGet]
@@ -24,5 +26,40 @@ public class JobController : Controller
             ListJob = result.Data.ToList();
         }
         return View(ListJob);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Create()
+    {
+        // get companies
+        var resultCompany = await _companyRepository.Get();
+        var listCompanyDtoGets = new List<CompanyDtoGet>();
+
+        if (resultCompany.Data != null)
+        {
+            listCompanyDtoGets = resultCompany.Data.ToList();
+        }
+
+        // add to view data
+        ViewData["Companies"] = listCompanyDtoGets;
+
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(JobDtoGet jobDtoPost)
+    {
+        var result = await _repository.Post(jobDtoPost);
+        if (result.Status == "200")
+        {
+            TempData["Success"] = "Data success created";
+            return RedirectToAction(nameof(Index));
+        }
+        else if (result.Status == "409")
+        {
+            ModelState.AddModelError(string.Empty, result.Message);
+            return View();
+        }
+        return RedirectToAction(nameof(Index));
     }
 }
