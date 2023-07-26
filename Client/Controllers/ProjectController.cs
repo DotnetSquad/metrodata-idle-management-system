a5468f96e1cfd1ab50a8581d486682ae3fef4e6a
aefshaeful <aefsaefulrohma042@gmail.com>
2023-07-26 11:22:21 +0700

    feat: create view create project feature

--- Client/Controllers/ProjectController.cs
+++ Client/Controllers/ProjectController.cs
@@ -1,5 +1,6 @@
 using Client.Contracts;
 using Client.DataTransferObjects.Projects;
+using Client.DataTransferObjects.Roles;
 using Microsoft.AspNetCore.Mvc;public class ProjectController : Controller
 {
     private readonly IProjectRepository _repository;
@@ -20,5 +21,28 @@ using Microsoft.AspNetCore.Mvc;public class ProjectController : Controller
             ListProject = result.Data.ToList();
         }
         return View(ListProject);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(ProjectDtoGet projectDtoPost)
+    {
+        var result = await _repository.Post(projectDtoPost);
+        if (result.Status == "200")
+        {
+            TempData["Success"] = "Data success created";
+            return RedirectToAction(nameof(Index));
+        }
+        else if (result.Status == "409")
+        {
+            ModelState.AddModelError(string.Empty, result.Message);
+            return View();
+        }
+        return RedirectToAction(nameof(Index));
     }
 }
