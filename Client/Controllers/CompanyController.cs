4c8a3f9be0a01f853a193999a81cb41cd6664242
putuagra <iputuagra@gmail.com>
2023-07-26 20:25:47 +0700

    feat: create view update company

--- Client/Controllers/CompanyController.cs
+++ Client/Controllers/CompanyController.cs
@@ -49,4 +49,43 @@ public class CompanyController : Controller
         }
         return RedirectToAction(nameof(Index));
     }
+
+    [HttpGet]
+    public async Task<IActionResult> Update(Guid guid)
+    {
+        var result = await _companyRepository.Get(guid);
+        var company = new CompanyDtoGet();
+        if (result.Data?.Guid is null)
+        {
+            return View(company);
+        }
+        else
+        {
+            company.Guid = result.Data.Guid;
+            company.CompanyName = result.Data.CompanyName;
+            company.Description = result.Data.Description;
+            company.Address = result.Data.Address;
+        }
+
+        return View(company);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update(Guid id, CompanyDtoGet companyDtoGet)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _companyRepository.Put(companyDtoGet.Guid, companyDtoGet);
+            if (result.Code == 200)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else if (result.Code == 409)
+            {
+                ModelState.AddModelError(string.Empty, result.Message);
+                return View();
+            }
+        }
+        return View();
+    }
 }
