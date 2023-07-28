8ddc0609d68d6ba99e9d377122d0169829365432
putuagra <iputuagra@gmail.com>
2023-07-28 23:37:59 +0700

    feat: create sweet alert delete project (#362)
    * feat: create sweet alert delete project
    
    * refactor: delete sweet alert for create
    
    * refactor: delete comment button

--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -3,3 +3,34 @@
 
 // Write your JavaScript code.
 new DataTable('#data-table');
+document.addEventListener('DOMContentLoaded', function () {
+    const deleteButtons = document.querySelectorAll('.delete-button');
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', function (event) {
+            event.preventDefault(); // Prevent the default form submission
+            const form = event.target.closest('form');
+            const guid = form.querySelector('input[name="guid"]').value;
+
+            Swal.fire({
+                title: 'Are you sure?',
+                text: 'You won\'t be able to recover this data!',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Yes, delete it!',
+                cancelButtonText: 'No, cancel',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    // If the user confirms, submit the form
+                    const form = event.target.closest('form');
+                    form.submit();
+                } else {
+                    Swal.fire(
+                        'Cancelled',
+                        'Your data is safe :)',
+                        'info'
+                    );
+                }
+            });
+        });
+    });
+});
