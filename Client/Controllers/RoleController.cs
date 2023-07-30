817976943f09da2167c16621ceeeea72c6835993
rifkymaulana <rifkymaulanaindonesia@gmail.com>
2023-07-30 07:33:23 +0700

    feat: change name variable

--- Client/Controllers/RoleController.cs
+++ Client/Controllers/RoleController.cs
@@ -19,10 +19,10 @@ public class RoleController : Controller
     [HttpGet]
     public async Task<IActionResult> Index()
     {
-        var result = await _roleRepository.Get();
+        var roles = await _roleRepository.Get();
         var listRoles = new List<RoleDtoGet>();
 
-        if (result.Data is not null) listRoles = result.Data.ToList();
+        if (roles.Data is not null) listRoles = roles.Data.ToList();
 
         return View(listRoles);
     }
@@ -36,18 +36,18 @@ public class RoleController : Controller
     [HttpPost]
     public async Task<IActionResult> Create(RoleDtoGet roleDtoPost)
     {
-        var result = await _roleRepository.Post(roleDtoPost);
+        var role = await _roleRepository.Post(roleDtoPost);
 
-        switch (result.Code)
+        switch (role.Code)
         {
             case 200:
-                TempData["Success"] = result.Message;
+                TempData["Success"] = role.Message;
                 return RedirectToAction(nameof(Index));
             case 400:
-                TempData["Error"] = result.Message;
+                TempData["Error"] = role.Message;
                 return RedirectToAction(nameof(Index));
             default:
-                TempData["Error"] = result.Message;
+                TempData["Error"] = role.Message;
                 return RedirectToAction(nameof(Index));
         }
     }
@@ -55,20 +55,20 @@ public class RoleController : Controller
     [HttpGet]
     public async Task<IActionResult> Update(Guid guid)
     {
-        var result = await _roleRepository.Get(guid);
-        var role = new RoleDtoGet();
+        var role = await _roleRepository.Get(guid);
+        var roleDtoGet = new RoleDtoGet();
 
-        switch (result.Code)
+        switch (role.Code)
         {
             case 200:
-                role.Guid = result.Data!.Guid;
-                role.Name = result.Data!.Name;
-                return View(role);
+                roleDtoGet.Guid = role.Data!.Guid;
+                roleDtoGet.Name = role.Data!.Name;
+                return View(roleDtoGet);
             case 400:
-                TempData["Error"] = result.Message;
+                TempData["Error"] = role.Message;
                 return RedirectToAction(nameof(Index));
             default:
-                TempData["Error"] = result.Message;
+                TempData["Error"] = role.Message;
                 return RedirectToAction(nameof(Index));
         }
     }
@@ -76,21 +76,21 @@ public class RoleController : Controller
     [HttpPost]
     public async Task<IActionResult> Update(Guid id, RoleDtoGet roleDtoGet)
     {
-        var result = await _roleRepository.Put(roleDtoGet.Guid, roleDtoGet);
+        var role = await _roleRepository.Put(roleDtoGet.Guid, roleDtoGet);
 
-        switch (result.Code)
+        switch (role.Code)
         {
             case 200:
-                TempData["Success"] = result.Message;
+                TempData["Success"] = role.Message;
                 return RedirectToAction(nameof(Index));
             case 400:
-                TempData["Error"] = result.Message;
+                TempData["Error"] = role.Message;
                 return RedirectToAction(nameof(Index));
             case 404:
-                TempData["Error"] = result.Message;
+                TempData["Error"] = role.Message;
                 return RedirectToAction(nameof(Index));
             default:
-                TempData["Error"] = result.Message;
+                TempData["Error"] = role.Message;
                 return RedirectToAction(nameof(Index));
         }
     }
@@ -98,21 +98,21 @@ public class RoleController : Controller
     [HttpPost]
     public async Task<IActionResult> Delete(Guid guid)
     {
-        var result = await _roleRepository.Delete(guid);
+        var role = await _roleRepository.Delete(guid);
 
-        switch (result.Code)
+        switch (role.Code)
         {
             case 200:
-                TempData["Success"] = result.Message;
+                TempData["Success"] = role.Message;
                 return RedirectToAction(nameof(Index));
             case 500:
-                TempData["Error"] = result.Message;
+                TempData["Error"] = role.Message;
                 return RedirectToAction(nameof(Index));
             case 404:
-                TempData["Error"] = result.Message;
+                TempData["Error"] = role.Message;
                 return RedirectToAction(nameof(Index));
             default:
-                TempData["Error"] = result.Message;
+                TempData["Error"] = role.Message;
                 return RedirectToAction(nameof(Index));
         }
     }
