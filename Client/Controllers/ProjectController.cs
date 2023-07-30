a87a3c9ca79d8dae1b19126c20ab1797ed345ae7
aefshaeful <105156900+aefshaeful@users.noreply.github.com>
2023-07-30 22:48:26 +0700

    feat: handle response handler from API in project controller client (#428)

--- Client/Controllers/ProjectController.cs
+++ Client/Controllers/ProjectController.cs
@@ -9,12 +9,12 @@ namespace Client.Controllers;
 
 public class ProjectController : Controller
 {
-    private readonly IProjectRepository _repository;
+    private readonly IProjectRepository _projectRepository;
     private readonly IEmployeeRepository _employeeRepository;
 
-    public ProjectController(IProjectRepository repository, IEmployeeRepository employeeRepository)
+    public ProjectController(IProjectRepository projectRepository, IEmployeeRepository employeeRepository)
     {
-        _repository = repository;
+        _projectRepository = projectRepository;
         _employeeRepository = employeeRepository;
     }
 
@@ -23,15 +23,25 @@ public class ProjectController : Controller
     [HttpGet]
     public async Task<IActionResult> Index()
     {
-        var result = await _repository.Get();
-        var ListProject = new List<ProjectDtoGet>();
+        var projects = await _projectRepository.Get();
+        var listProjectDtoGets = new List<ProjectDtoGet>();
 
-        if (result.Data != null)
+        if (projects.Data is not null)
         {
-            ListProject = result.Data.ToList();
+            listProjectDtoGets = projects.Data.ToList();
         }
 
-        return View(ListProject);
+        var employees = await _employeeRepository.Get();
+        var listEmployeeDtoGets = new List<EmployeeDtoGet>();
+
+        if (employees.Data is not null)
+        {
+            listEmployeeDtoGets = employees.Data.ToList();
+        }
+
+        ViewData["Employees"] = listEmployeeDtoGets;
+
+        return View(listProjectDtoGets);
     }
 
     [Authorize(Roles = $"{nameof(RoleLevelEnum.Manager)}, {nameof(RoleLevelEnum.Admin)}")]
@@ -39,12 +49,12 @@ public class ProjectController : Controller
     public async Task<IActionResult> Create()
     {
         // get employees
-        var result = await _employeeRepository.Get();
+        var employees = await _employeeRepository.Get();
         var listEmployeeDtoGets = new List<EmployeeDtoGet>();
 
-        if (result.Data != null)
+        if (employees.Data is not null)
         {
-            listEmployeeDtoGets = result.Data.ToList();
+            listEmployeeDtoGets = employees.Data.ToList();
         }
 
         // add to view data
@@ -56,19 +66,20 @@ public class ProjectController : Controller
     [HttpPost]
     public async Task<IActionResult> Create(ProjectDtoGet projectDtoPost)
     {
-        var result = await _repository.Post(projectDtoPost);
-        if (result.Status == "200")
-        {
-            TempData["Success"] = "Data success created";
-            return RedirectToAction(nameof(Index));
-        }
-        else if (result.Status == "409")
-        {
-            ModelState.AddModelError(string.Empty, result.Message);
-            return View();
-        }
+        var project = await _projectRepository.Post(projectDtoPost);
 
-        return RedirectToAction(nameof(Index));
+        switch (project.Code)
+        {
+            case 201:
+                TempData["Success"] = project.Message;
+                return RedirectToAction(nameof(Index));
+            case 400:
+                TempData["Error"] = project.Message;
+                return RedirectToAction(nameof(Index));
+            default:
+                TempData["Error"] = project.Message;
+                return RedirectToAction(nameof(Index));
+        }
     }
 
     [Authorize(Roles = $"{nameof(RoleLevelEnum.Manager)}, {nameof(RoleLevelEnum.Admin)}")]
@@ -76,65 +87,79 @@ public class ProjectController : Controller
     public async Task<IActionResult> Update(Guid guid)
     {
         // get employees
-        var result = await _employeeRepository.Get();
+        var employees = await _employeeRepository.Get();
         var listEmployeeDtoGets = new List<EmployeeDtoGet>();
 
-        if (result.Data != null)
+        if (employees.Data != null)
         {
-            listEmployeeDtoGets = result.Data.ToList();
+            listEmployeeDtoGets = employees.Data.ToList();
         }
 
         // add to view data
         ViewData["Employees"] = listEmployeeDtoGets;
 
-        var value = await _repository.Get(guid);
-        var project = new ProjectDtoGet();
-        if (value.Data?.Guid is null)
+        var project = await _projectRepository.Get(guid);
+        var projectDtoGet = new ProjectDtoGet();
+
+        if (project.Data is null)
         {
-            return View(project);
+            TempData["Error"] = project.Message;
+            return RedirectToAction(nameof(Index));
         }
         else
         {
-            project.Guid = value.Data.Guid;
-            project.NameProject = value.Data.NameProject;
-            project.ProjectLead = value.Data.ProjectLead;
-            project.Description = value.Data.Description;
+            projectDtoGet.Guid = project.Data.Guid;
+            projectDtoGet.NameProject = project.Data.NameProject;
+            projectDtoGet.ProjectLead = project.Data.ProjectLead;
+            projectDtoGet.Description = project.Data.Description;
         }
 
-        return View(project);
+        return View(projectDtoGet);
     }
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Update(ProjectDtoGet project)
+    public async Task<IActionResult> Update(ProjectDtoGet projectDtoGet)
     {
-        if (ModelState.IsValid)
-        {
-            var result = await _repository.Put(project.Guid, project);
-            if (result.Code == 200)
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            else if (result.Status == "409")
-            {
-                ModelState.AddModelError(string.Empty, result.Message);
-                return View();
-            }
-        }
+        var project = await _projectRepository.Put(projectDtoGet.Guid, projectDtoGet);
 
-        return View();
+        switch (project.Code)
+        {
+            case 200:
+                TempData["Success"] = project.Message;
+                return RedirectToAction(nameof(Index));
+            case 400:
+                TempData["Error"] = project.Message;
+                return RedirectToAction(nameof(Index));
+            case 404:
+                TempData["Error"] = project.Message;
+                return RedirectToAction(nameof(Index));
+            default:
+                TempData["Error"] = project.Message;
+                return RedirectToAction(nameof(Index));
+        }
     }
 
     [Authorize(Roles = $"{nameof(RoleLevelEnum.Manager)}, {nameof(RoleLevelEnum.Admin)}")]
     [HttpPost]
     public async Task<IActionResult> Delete(Guid guid)
     {
-        var result = await _repository.Delete(guid);
-        if (result.Code == 200)
-        {
-            return RedirectToAction(nameof(Index));
-        }
+        var project = await _projectRepository.Delete(guid);
 
-        return RedirectToAction(nameof(Index));
+        switch (project.Code)
+        {
+            case 200:
+                TempData["Success"] = project.Message;
+                return RedirectToAction(nameof(Index));
+            case 404:
+                TempData["Error"] = project.Message;
+                return RedirectToAction(nameof(Index));
+            case 500:
+                TempData["Error"] = project.Message;
+                return RedirectToAction(nameof(Index));
+            default:
+                TempData["Error"] = project.Message;
+                return RedirectToAction(nameof(Index));
+        }
     }
 }
