135b6958692438a7e01685d336a57fdc813c2a57
Rifky Maulana <rifkymaulanaindonesia@gmail.com>
2023-08-02 14:28:47 +0700

    feat: add coloum status approval in table employee project and employee interview (#489)

---
+++ API/Migrations/20230802072152_add coloum status approval.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class addcoloumstatusapproval : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "status_approval",
+                table: "tb_tr_employee_project",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "status_approval",
+                table: "tb_tr_employee_interview",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "is_used",
+                table: "tb_m_accounts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "status_approval",
+                table: "tb_tr_employee_project");
+
+            migrationBuilder.DropColumn(
+                name: "status_approval",
+                table: "tb_tr_employee_interview");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "is_used",
+                table: "tb_m_accounts",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
---
+++ API/Utilities/Enums/StatusApprovalEnum.cs
@@ -0,0 +1,8 @@
+namespace API.Utilities.Enums;
+
+public enum StatusApprovalEnum
+{
+    Pending,
+    Accepted,
+    Rejected
+}
