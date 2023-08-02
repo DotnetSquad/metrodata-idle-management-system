b1a3eb10af9a31b40c06138b3dad6e123eaa317d
putuagra <iputuagra@gmail.com>
2023-08-02 23:06:08 +0700

    feat: set view employee interview in client

--- Client/DataTransferObjects/EmployeeInterviews/EmployeeInterviewDtoCreate.cs
+++ Client/DataTransferObjects/EmployeeInterviews/EmployeeInterviewDtoCreate.cs
@@ -1,7 +1,10 @@
+using Client.Utilities.Enums;
+
 namespace Client.DataTransferObjects.EmployeeInterviews;
 
 public class EmployeeInterviewDtoCreate
 {
     public Guid EmployeeGuid { get; set; }
     public Guid InterviewGuid { get; set; }
+    public StatusApprovalEnum StatusApproval { get; set; }
 }
