ca4e5d106389c02393d7b1e4b48d905bcc95ae71
putuagra <iputuagra@gmail.com>
2023-08-03 16:51:59 +0700

    feat: create grade details employee (#526)

--- Client/Controllers/GradeController.cs
+++ Client/Controllers/GradeController.cs
@@ -1,4 +1,5 @@
 ﻿using Client.Contracts;
+using Client.DataTransferObjects.Employees;
 using Client.DataTransferObjects.Grades;
 using Client.Utilities.Enums;
 using Microsoft.AspNetCore.Authorization;
@@ -10,10 +11,12 @@ public class GradeController : Controller
 {
     public string isNotCollapsed = "GradeController";
     private readonly IGradeRepository _gradeRepository;
+    private readonly IEmployeeRepository _employeeRepository;
 
-    public GradeController(IGradeRepository gradeRepository)
+    public GradeController(IGradeRepository gradeRepository, IEmployeeRepository employeeRepository)
     {
         _gradeRepository = gradeRepository;
+        _employeeRepository = employeeRepository;
     }
 
     [Authorize(Roles =
@@ -30,6 +33,12 @@ public class GradeController : Controller
             ListGrades = grades.Data.ToList();
         }
 
+        var employees = await _employeeRepository.Get();
+        var listEmployeeDtoGets = new List<EmployeeDtoGet>();
+
+        if (employees.Data is not null) listEmployeeDtoGets = employees.Data.ToList();
+
+        ViewData["Employees"] = listEmployeeDtoGets;
         ViewData["isNotCollapsed"] = isNotCollapsed;
         return View(ListGrades);
     }
@@ -131,6 +140,47 @@ public class GradeController : Controller
                 return RedirectToAction(nameof(Index));
             default:
                 TempData["Error"] = "Failed to delete grade.";
+                return RedirectToAction(nameof(Index));
+        }
+    }
+
+    [Authorize(Roles = $"{nameof(RoleLevelEnum.Employee)}")]
+    [HttpGet]
+    public async Task<IActionResult> Details(Guid guid)
+    {
+        var employeeGuid = User.Claims.FirstOrDefault(x => x.Type == "Guid")?.Value;
+        var employeeGuidTemp = Guid.Parse(employeeGuid);
+
+        var grade = await _gradeRepository.Get(guid);
+        var gradeDtoGet = new GradeDtoGet();
+
+        var employee = await _employeeRepository.Get(employeeGuidTemp);
+        var employeeDtoGets = new EmployeeDtoGet();
+
+        if (employee.Data is not null)
+        {
+            employeeDtoGets = employee.Data;
+        }
+
+        ViewData["isNotCollapsed"] = isNotCollapsed;
+        ViewData["Employee"] = employeeDtoGets;
+
+        switch (grade.Code)
+        {
+            case 200:
+                gradeDtoGet.Guid = grade.Data!.Guid;
+                gradeDtoGet.GradeLevel = grade.Data!.GradeLevel;
+                gradeDtoGet.ScoreSegment1 = grade.Data!.ScoreSegment1;
+                gradeDtoGet.ScoreSegment2 = grade.Data!.ScoreSegment2;
+                gradeDtoGet.ScoreSegment3 = grade.Data!.ScoreSegment3;
+                gradeDtoGet.ScoreSegment4 = grade.Data!.ScoreSegment4;
+                gradeDtoGet.TotalScore = grade.Data!.TotalScore;
+                return View(gradeDtoGet);
+            case 400:
+                TempData["Error"] = grade.Message;
+                return RedirectToAction(nameof(Index));
+            default:
+                TempData["Error"] = "Data grade not found.";
                 return RedirectToAction(nameof(Index));
         }
     }
