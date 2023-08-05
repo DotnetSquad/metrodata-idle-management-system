a5c4bcf914156e9416d0641da2b7151905d2a02d
putuagra <iputuagra@gmail.com>
2023-08-05 22:48:17 +0700

    feat: create end point for 5 top client (#579)

--- API/Controllers/DashboardController.cs
+++ API/Controllers/DashboardController.cs
@@ -64,4 +64,52 @@ public class DashboardController : ControllerBase
             Data = status
         });
     }
+
+    [HttpGet("GetProjectStatus")]
+    public IActionResult GetProjectStatus()
+    {
+        var status = _dashboardService.GetProjectStatus();
+        if (status is null)
+        {
+            return NotFound(new ResponseHandler<DashboardDtoGetInterviewStatus>
+            {
+                Code = StatusCodes.Status404NotFound,
+                Status = HttpStatusCode.NotFound.ToString(),
+                Message = "No project status found",
+                Data = null
+            });
+        }
+
+        return Ok(new ResponseHandler<DashboardDtoGetInterviewStatus>
+        {
+            Code = StatusCodes.Status200OK,
+            Status = HttpStatusCode.OK.ToString(),
+            Message = "Project status found",
+            Data = status
+        });
+    }
+
+    [HttpGet("GetTop5Client")]
+    public IActionResult GetTop5Client()
+    {
+        var clients = _dashboardService.GetTop5Client();
+        if (!clients.Any())
+        {
+            return NotFound(new ResponseHandler<DashboardDtoGetClient>
+            {
+                Code = StatusCodes.Status404NotFound,
+                Status = HttpStatusCode.NotFound.ToString(),
+                Message = "No client found",
+                Data = null
+            });
+        }
+
+        return Ok(new ResponseHandler<IEnumerable<DashboardDtoGetClient>>
+        {
+            Code = StatusCodes.Status200OK,
+            Status = HttpStatusCode.OK.ToString(),
+            Message = "Clients found",
+            Data = clients
+        });
+    }
 }
--- API/Services/DashboardService.cs
+++ API/Services/DashboardService.cs
@@ -7,14 +7,22 @@ namespace API.Services;
 public class DashboardService
 {
     private readonly IEmployeeRepository _employeeRepository;
-    private readonly IEmployeeJobRepository _employeeInterviewRepository;
+    private readonly IEmployeeJobRepository _employeeJobRepository;
     private readonly IInterviewRepository _interviewRepository;
+    private readonly IEmployeeProjectRepository _employeeProjectRepository;
+    private readonly ICompanyRepository _companyRepository;
+    private readonly IProjectRepository _projectRepository;
+    private readonly IPlacementRepository _placementRepository;
 
-    public DashboardService(IEmployeeRepository employeeRepository, IInterviewRepository interviewRepository, IEmployeeJobRepository employeeInterviewRepository)
+    public DashboardService(IEmployeeRepository employeeRepository, IInterviewRepository interviewRepository, IEmployeeJobRepository employeeJobRepository, IProjectRepository projectRepository, IEmployeeProjectRepository employeeProjectRepository, ICompanyRepository companyRepository, IPlacementRepository placementRepository)
     {
         _employeeRepository = employeeRepository;
         _interviewRepository = interviewRepository;
-        _employeeInterviewRepository = employeeInterviewRepository;
+        _employeeJobRepository = employeeJobRepository;
+        _projectRepository = projectRepository;
+        _employeeProjectRepository = employeeProjectRepository;
+        _companyRepository = companyRepository;
+        _placementRepository = placementRepository;
     }
 
     public DashboardsDtoGetStatus GetEmployeeStatus()
@@ -41,13 +49,13 @@ public class DashboardService
     {
 
         var interviewStatus = (from i in _interviewRepository.GetAll()
-                               join er in _employeeInterviewRepository.GetAll() on i.Guid equals er.InterviewGuid
-                               join e in _employeeRepository.GetAll() on er.EmployeeGuid equals e.Guid
+                               join ej in _employeeJobRepository.GetAll() on i.Guid equals ej.InterviewGuid
+                               join e in _employeeRepository.GetAll() on ej.EmployeeGuid equals e.Guid
                                select new DashboardDtoGetInterviewStatus
                                {
-                                   Accepted = er.StatusApproval == StatusApprovalEnum.Accepted ? 1 : 0,
-                                   Pending = er.StatusApproval == StatusApprovalEnum.Pending ? 1 : 0,
-                                   Rejected = er.StatusApproval == StatusApprovalEnum.Rejected ? 1 : 0
+                                   Accepted = ej.StatusApproval == StatusApprovalEnum.Accepted ? 1 : 0,
+                                   Pending = ej.StatusApproval == StatusApprovalEnum.Pending ? 1 : 0,
+                                   Rejected = ej.StatusApproval == StatusApprovalEnum.Rejected ? 1 : 0
                                });
 
         if (!interviewStatus.Any())
@@ -63,5 +71,48 @@ public class DashboardService
         };
 
         return status;
+    }
+
+    public DashboardDtoGetInterviewStatus GetProjectStatus()
+    {
+
+        var interviewStatus = (from p in _projectRepository.GetAll()
+                               join ep in _employeeProjectRepository.GetAll() on p.Guid equals ep.ProjectGuid
+                               join e in _employeeRepository.GetAll() on ep.EmployeeGuid equals e.Guid
+                               select new DashboardDtoGetInterviewStatus
+                               {
+                                   Accepted = ep.StatusApproval == StatusApprovalEnum.Accepted ? 1 : 0,
+                                   Pending = ep.StatusApproval == StatusApprovalEnum.Pending ? 1 : 0,
+                                   Rejected = ep.StatusApproval == StatusApprovalEnum.Rejected ? 1 : 0
+                               });
+
+        if (!interviewStatus.Any())
+        {
+            return null;
+        }
+
+        var status = new DashboardDtoGetInterviewStatus
+        {
+            Accepted = interviewStatus.Sum(i => i.Accepted),
+            Pending = interviewStatus.Sum(i => i.Pending),
+            Rejected = interviewStatus.Sum(i => i.Rejected)
+        };
+
+        return status;
+    }
+
+    public IEnumerable<DashboardDtoGetClient> GetTop5Client()
+    {
+        var getClient = _placementRepository.GetAll()
+            .GroupBy(pl => pl.CompanyGuid)
+            .Select(group => new DashboardDtoGetClient
+            {
+                CompanyGuid = group.Key,
+                TotalEmployees = group.Count()
+            })
+            .OrderByDescending(x => x.TotalEmployees)
+            .Take(5);
+
+        return getClient;
     }
 }
