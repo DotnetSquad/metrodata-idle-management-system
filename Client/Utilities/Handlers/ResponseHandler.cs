41b9c7561f62cf679f117f84083fe9ccb6bd2d31
Rifky Maulana <rifkymaulanaindonesia@gmail.com>
2023-08-05 16:46:58 +0700

    feat: handle fluent validation (#563)

--- Client/Utilities/Handlers/ResponseHandler.cs
+++ Client/Utilities/Handlers/ResponseHandler.cs
@@ -3,7 +3,20 @@
 public class ResponseHandler<TEntity>
 {
     public int Code { get; set; }
-    public string Status { get; set; }
+    public string StatusAPI { get; set; }
     public string Message { get; set; }
     public TEntity? Data { get; set; }
+    public string Type { get; set; }
+    public string Title { get; set; }
+    public int Status { get; set; }
+    public string TraceId { get; set; }
+    public Errors Errors { get; set; }
+}
+
+public class Errors
+{
+    public string[] Email { get; set; }
+    public string[] Password { get; set; }
+    public string[] HiringDate{ get; set; }
+    public string[] PhoneNumber { get; set; }
 }
