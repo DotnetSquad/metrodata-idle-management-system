759f7f5509e215bd5026ff5f7079c863ab5bf871
Rifky Maulana <rifkymaulanaindonesia@gmail.com>
2023-08-05 11:47:52 +0700

    feat: approval and reject behavior feature (#551)

--- API/Services/EmployeeJobService.cs
+++ API/Services/EmployeeJobService.cs
@@ -1,16 +1,24 @@
 using API.Contracts;
 using API.DataTransferObjects.EmployeeJobs;
 using API.DataTransferObjects.EmployeeProjects;
+using API.DataTransferObjects.Placements;
+using API.Utilities.Enums;
 
 namespace API.Services;
 
 public class EmployeeJobService
 {
+    private readonly ICompanyRepository _companyRepository;
     private readonly IEmployeeJobRepository _employeeJobRepository;
+    private readonly IPlacementRepository _placementRepository;
+    private readonly IJobRepository _jobRepository;
 
-    public EmployeeJobService(IEmployeeJobRepository employeeJobRepository)
+    public EmployeeJobService(ICompanyRepository companyRepository, IEmployeeJobRepository employeeJobRepository, IPlacementRepository placementRepository, IJobRepository jobRepository)
     {
+        _companyRepository = companyRepository;
         _employeeJobRepository = employeeJobRepository;
+        _placementRepository = placementRepository;
+        _jobRepository = jobRepository;
     }
 
     public IEnumerable<EmployeeJobDtoGet> Get()
@@ -49,7 +57,34 @@ public class EmployeeJobService
         if (employeeJob is null) return -1;
 
         var employeeJobUpdated = _employeeJobRepository.Update(employeeJobDtoUpdate);
-        return !employeeJobUpdated ? 0 : 1;
+        var job = _jobRepository.GetByGuid(employeeJobDtoUpdate.JobGuid);
+        var company = _companyRepository.GetByGuid(job.CompanyGuid);
+        if (employeeJobUpdated)
+        {
+            if (employeeJobDtoUpdate.StatusApproval == StatusApprovalEnum.Accepted)
+            {
+                var placementDtoCreate = new PlacementDtoCreate
+                {
+                    Title = $"{job.JobName} at {company.CompanyName}",
+                    Description = job.Description,
+                    EmployeeGuid = employeeJobDtoUpdate.EmployeeGuid,
+                    CompanyGuid = company.Guid
+                };
+                var placementCreated = _placementRepository.Create(placementDtoCreate);
+            }
+            
+            if (employeeJobDtoUpdate.StatusApproval == StatusApprovalEnum.Rejected)
+            {
+                var placement = _placementRepository.GetByEmployeeGuid(employeeJobDtoUpdate.EmployeeGuid);
+                var placemrntDtoGets = (List<PlacementDtoGet>)placement;
+                foreach (var placementDtoGet in placemrntDtoGets)
+                {
+                    if (placementDtoGet.EmployeeGuid == employeeJobDtoUpdate.EmployeeGuid && placementDtoGet.CompanyGuid == company.Guid) _placementRepository.Delete(placementDtoGet);
+                    
+                }
+            }
+        }
+        return employeeJobUpdated ? 1 : 0;
     }
 
     public int Delete(Guid guid)
@@ -58,6 +93,21 @@ public class EmployeeJobService
         if (employeeJob is null) return -1;
 
         var employeeJobDeleted = _employeeJobRepository.Delete(employeeJob);
+        
+        var placement = _placementRepository.GetByEmployeeGuid(employeeJob.EmployeeGuid);
+        var placementDtoGets = (List<PlacementDtoGet>)placement;
+        
+        var job = _jobRepository.GetByGuid(employeeJob.JobGuid);
+        var company = _companyRepository.GetByGuid(job.CompanyGuid);
+
+        if (employeeJobDeleted)
+        {
+            foreach (var placementDtoGet in placementDtoGets)
+            {
+                if (placementDtoGet.EmployeeGuid == employeeJob.EmployeeGuid && placementDtoGet.CompanyGuid == company.Guid) _placementRepository.Delete(placementDtoGet);
+                    
+            }
+        }
         return !employeeJobDeleted ? 0 : 1;
     }
     
