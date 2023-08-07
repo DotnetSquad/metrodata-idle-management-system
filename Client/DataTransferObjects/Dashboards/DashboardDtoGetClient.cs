b63fb3fb8061e83099c71969c3fa37253a16f4e0
Rifky Maulana <rifkymaulanaindonesia@gmail.com>
2023-08-07 09:51:49 +0700

    Revert "feat: create top client dashboard (#599)" (#600)
    This reverts commit ff5fd66cfe6aa9b58e7007e863e139993c244586.

--- Client/DataTransferObjects/Dashboards/DashboardDtoGetClient.cs
+++
@@ -1,8 +0,0 @@
-namespace Client.DataTransferObjects.Dashboards;
-
-public class DashboardDtoGetClient
-{
-    public Guid CompanyGuid { get; set; }
-    public string CompanyName { get; set; }
-    public int TotalEmployees { get; set; }
-}
